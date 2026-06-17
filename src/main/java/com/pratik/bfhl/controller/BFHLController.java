66708419b07d7e79cdd9e08e1977e4cb09595ee6
Pratik Shinde <shindepratik812@gmail.com>
2026-06-17 21:46:05 +0530

    Fix controller imports

--- src/main/java/com/pratik/bfhl/controller/BFHLController.java
+++ src/main/java/com/pratik/bfhl/controller/BFHLController.java
@@ -3,15 +3,9 @@ package com.pratik.bfhl.controller;
 import com.pratik.bfhl.dto.RequestDTO;
 import com.pratik.bfhl.dto.ResponseDTO;
 import com.pratik.bfhl.service.BFHLService;
-
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +16,11 @@ public class BFHLController {
     @GetMapping("/")
     public String home() {
         return "BFHL API Running Successfully";
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("UP");
     }
 
     @PostMapping("/bfhl")
