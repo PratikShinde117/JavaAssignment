391238adf65495a4f21990a3876ba86598065291
Pratik Shinde <shindepratik812@gmail.com>
2026-06-17 18:14:19 +0530

    Add health check endpoint

--- src/main/java/com/pratik/bfhl/controller/BFHLController.java
+++ src/main/java/com/pratik/bfhl/controller/BFHLController.java
@@ -1,26 +1,19 @@
-package com.pratik.bfhl.controller;
-
-import com.pratik.bfhl.dto.RequestDTO;
-import com.pratik.bfhl.dto.ResponseDTO;
-import com.pratik.bfhl.service.BFHLService;
-
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 @RestController
 @RequiredArgsConstructor
 public class BFHLController {
 
     private final BFHLService service;
 
+    @GetMapping("/")
+    public String home() {
+        return "BFHL API Running Successfully";
+    }
+
     @PostMapping("/bfhl")
     public ResponseEntity<ResponseDTO> process(
             @RequestBody RequestDTO request,
             @RequestHeader("X-Request-Id") String requestId
     ) {
-
         long start = System.currentTimeMillis();
 
         return ResponseEntity.ok(
